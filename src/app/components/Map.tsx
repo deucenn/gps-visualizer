a86a3068e16ecc5d7bc07eccaff78562912b0502
deucenn <panix.black@web.de>
2025-01-09 11:38:26 +0100

    chore. improved Map.tsx

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,40 +1,49 @@
-"use client"
+"use client";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { LatLngExpression, LatLngTuple } from 'leaflet';
-
+import { LatLngExpression } from "leaflet";
 import "leaflet/dist/leaflet.css";
-import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
-import "leaflet-defaulticon-compatibility";
+import L from "leaflet";
+
+// Fix for Leaflet's default icons
+const iconPrototype = L.Icon.Default.prototype as L.Icon & {
+  _getIconUrl?: () => void;
+};
+delete iconPrototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+  iconUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+  shadowUrl:
+    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+});
 
 interface MapProps {
-    posix: LatLngExpression | LatLngTuple,
-    zoom?: number,
+  markers: LatLngExpression[];
 }
 
-const defaults = {
-    zoom: 19,
-}
+const Map = ({ markers }: MapProps) => {
+  return (
+    <MapContainer
+      center={markers[0]} // Center the map on the first marker
+      zoom={13}
+      scrollWheelZoom={true}
+      style={{ height: "100%", width: "100%" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {markers.map((position, index) => (
+        <Marker key={index} position={position}>
+          <Popup>Marker #{index + 1}</Popup>
+        </Marker>
+      ))}
+    </MapContainer>
+  );
+};
 
-const Map = (Map: MapProps) => {
-    const { zoom = defaults.zoom, posix } = Map
+export default Map;
 
-    return (
-        <MapContainer
-            center={posix}
-            zoom={zoom}
-            scrollWheelZoom={true}
-            style={{ height: "100%", width: "100%" }}
-        >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={posix} draggable={false}>
-                <Popup>Hey ! I study here</Popup>
-            </Marker>
-        </MapContainer>
-    )
-}
 
-export default Map
