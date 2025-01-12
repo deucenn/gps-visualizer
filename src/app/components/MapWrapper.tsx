18f67e4e44fd04700c7a69e38da52e2fbc6a1a37
deucenn <panix.black@web.de>
2025-01-12 13:42:27 +0100

    build: delete marker functionality

--- src/app/components/MapWrapper.tsx
+++ src/app/components/MapWrapper.tsx
@@ -7,6 +7,7 @@ import Modal from "@mui/material/Modal";
 import TextField from "@mui/material/TextField";
 import Avatar from "@mui/material/Avatar";
 import AddIcon from "@mui/icons-material/Add";
+import { Snackbar } from "@mui/material";
 
 const Map = dynamic(() => import("./Map"), {
   loading: () => <p>A map is loading...</p>,
@@ -28,9 +29,35 @@ export default function MapWrapper() {
   const [latitude, setLatitude] = useState("");
   const [longitude, setLongitude] = useState("");
   const [markerName, setMarkerName] = useState("");
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
+  const [deleteMarker, setDeleteMarker] = useState<MarkerData | null>(null);
+  const [snackbarMessage, setSnackbarMessage] = useState<string | null>(null);
 
   const handleOpen = () => setOpenModal(true);
   const handleClose = () => setOpenModal(false);
+
+  const handleDeleteModalOpen = (marker: MarkerData) => {
+    setOpenDeleteModal(true);
+    setDeleteMarker(marker);
+  };
+  const handleDeleteModalClose = () => {
+    setOpenDeleteModal(false);
+    setDeleteMarker(null);
+  };
+
+  const handleDeleteMarker = () => {
+    if (!deleteMarker) return;
+
+    const newPositions = positions.filter(
+      (marker) =>
+        marker.position[0] !== deleteMarker.position[0] &&
+        marker.position[1] !== deleteMarker.position[1]
+    );
+    setPositions(newPositions);
+    setOpenDeleteModal(false);
+    setDeleteMarker(null);
+    setSnackbarMessage(`Marker deleted: ${deleteMarker.name}`);
+  };
 
   const handleLatitudeChange = (event: React.ChangeEvent<HTMLInputElement>) =>
     setLatitude(event.target.value);
@@ -53,6 +80,9 @@ export default function MapWrapper() {
     setLongitude("");
     setMarkerName("");
     handleClose();
+
+    setSnackbarMessage(`Marker added: ${markerName}`);
+    setTimeout(() => setSnackbarMessage(null), 3000);
   };
 
   return (
@@ -172,18 +202,54 @@ export default function MapWrapper() {
       </Modal>
       <div className="pt-3 border-t-2">
         <h1 className="text-xl font-sans">All markers</h1>
-        <div className="grid grid-cols-4 gap-4">
+        <div className="grid grid-cols-1 gap-4 w-1/2">
           {positions.map((marker, index) => (
             <div
               key={index}
-              className="flex items-center justify-between p-2"
+              className="flex items-center justify-between p-2 hover:bg-slate-600 rounded transition hover:text-slate-900 duration-200 hover:cursor-pointer"
+              onClick={() => handleDeleteModalOpen(marker)}
             >
               <p className="font-medium">{marker.name}</p>
               <p className="text-gray-300">{`Lat: ${marker.position[0]}, Lng: ${marker.position[1]}`}</p>
             </div>
           ))}
         </div>
+
+        {/* Delete Modal */}
+        <Modal
+          open={openDeleteModal}
+          onClose={handleDeleteModalClose}
+          aria-labelledby="modal-title"
+          aria-describedby="modal-description"
+        >
+          <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-800 rounded-lg shadow-md p-6">
+            <h2 id="modal-title" className="text-lg font-semibold mb-2">
+              Do you really want to delete the marker "{deleteMarker?.name}"?
+            </h2>
+            <div className="flex justify-end mt-4">
+              <button
+                className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-700 transition duration-200"
+                onClick={handleDeleteMarker}
+              >
+                Delete
+              </button>
+              <button
+                className="ml-2 px-4 py-2 bg-gray-400 text-white rounded hover:bg-gray-500 transition duration-200"
+                onClick={handleDeleteModalClose}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </Modal>
       </div>
+
+      <Snackbar
+        open={!!snackbarMessage}
+        autoHideDuration={3000}
+        onClose={() => setSnackbarMessage(null)}
+        message={snackbarMessage}
+      />
     </div>
   );
 }
