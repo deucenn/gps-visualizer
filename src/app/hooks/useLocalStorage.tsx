0df4931a526566a0ef1d6a7bc9bafe724a2d876d
deucenn <panix.black@web.de>
2025-01-16 12:37:51 +0100

    chore: imrpoved checks

--- src/app/hooks/useLocalStorage.tsx
+++ src/app/hooks/useLocalStorage.tsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from "react";
 
 export default function useLocalStorage<T>(key: string, initialValue: T): [T, (value: T) => void] {
     const [storedValue, setStoredValue] = useState(() => {
-      try {
-        const item = window.localStorage.getItem(key);
-        return item ? JSON.parse(item) : initialValue;
-      } catch (error) {
-        console.error(error);
-        return initialValue;
-      }
+        try {
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        } catch (error) {
+            console.error(`Error reading from localStorage for key "${key}":`, error);
+            return initialValue;
+        }
     });
   
     useEffect(() => {
